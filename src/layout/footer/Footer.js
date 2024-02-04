4ae2aac0e8d8fb176e8fa1e390552c27a30c0da4
shahrukhnasir <shahrukhnasir0108@gmail.com>
2024-02-05 03:20:27 +0500

    header footer and hero section completed

--- src/layout/footer/Footer.js
+++ src/layout/footer/Footer.js
@@ -2,9 +2,10 @@ import { useState } from "react";
 import styles from "../footer/footer.module.css";
 import Link from "next/link";
 import Image from "next/image";
-import { FaInstagram, FaTiktok } from "react-icons/fa";
+import { FaInstagram, FaInstagramSquare, FaSnapchatGhost, FaTiktok } from "react-icons/fa";
 import { FaFacebookF } from "react-icons/fa";
 import { useRouter } from "next/router";
+import { FaXTwitter } from "react-icons/fa6";
 const Footer = () => {
 
 
@@ -16,16 +17,12 @@ const Footer = () => {
 					<div className="row">
 						<div className="col-lg-6 px-0">
 							<div className={styles.firstSection}>
-								<Link href="/">
-									{/* <img
-										src="/images/Groves_logo.png"
-										className="img-fluid"
-										alt="logo"
-									/> */}
-									<p className={styles.decs}>Join us for an	</p>
-									<p className={styles.decs}>unforgettable experience
-									</p>
-								</Link>
+
+
+								<p className={styles.decs}>Join us for an	</p>
+								<p className={styles.decs}>unforgettable experience
+								</p>
+
 
 
 							</div>
@@ -69,7 +66,7 @@ const Footer = () => {
 								</ul>
 							</div>
 						</div>
-						
+
 						<div className="col-lg-3 px-0">
 							<div className={`${styles.thirdSection}`}>
 								<ul className={`${styles.LinkSec} p-0`}>
@@ -100,14 +97,14 @@ const Footer = () => {
 								</ul>
 							</div>
 						</div>
-						
+
 						<div className="col-lg-3 px-0">
 							<div className={`${styles.thirdSection}`}>
 								<ul className={`${styles.LinkSec} p-0`}>
 
 									<li>
 										<Link href="/blogs" className={styles.footerLink}>
-											Guest Service call	
+											Guest Service call
 										</Link>
 									</li>	<li>
 										<Link href="/blogs" className={styles.footerLink}>
@@ -131,18 +128,65 @@ const Footer = () => {
 								</ul>
 							</div>
 						</div>
-						
+					</div>
+
+					<div className="row">
+						<div className="col-lg-8 px-0">
+							<div>
+
+
+								<img
+									src="/images/Groves_logo.png"
+									className="img-fluid py-3"
+									alt="logo"
+								/>
+							</div>
+							<div className="row">
+								<div className="col-3">
+									<p className={styles.copyRight}>
+										Term and Conditions
+									</p>
+
+								</div>
+
+								<div className="col-2">
+									<p className={styles.copyRight}>
+										Privacy Policy
+									</p>
+								</div>
+
+								<div className="col-4">
+									<p className={styles.copyRight}>
+										© 2023 The Groves for Entertainment
+									</p>
+								</div>
+							</div>
+
+						</div>
+						<div className="col-lg-4">
+							<div className={styles?.iconsSection}>
+								<ul className={styles?.TopLinks}>
+									<li> <FaTiktok className={styles?.icon} /></li>
+									<li> <FaInstagramSquare className={styles?.icon} /> </li>
+									<li> <FaXTwitter className={styles?.icon} /> </li>
+									<li><FaSnapchatGhost className={styles?.icon} /></li>
+								</ul>
+							</div>
+
+							<div className="text-end">
+
+								
+							<img
+									src="/images/visa.png"
+									className="img-fluid me-5"
+									alt="logo"
+								/>
+							</div>
+						</div>
 
 
 					</div>
 
-					<hr className={styles.line} />
-
-					<div className="text-center">
-						<p className={styles.copyRight}>
-							© 2023 Groves. All Right Reserved
-						</p>
-					</div>
 				</div>
 			</div>
 		</>
